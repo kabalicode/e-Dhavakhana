39c7e096265eada5952bf716b044f5bc5612e325
kabalicode <saibejugam@Sai-Shrawan.local>
2016-09-07 13:11:06 +0530

    Base version
    Initial version

---
+++ app/app.ts
@@ -0,0 +1,64 @@
+import {Component, ViewChild} from '@angular/core';
+import {ionicBootstrap, Platform, MenuController, Nav} from 'ionic-angular';
+import {StatusBar} from 'ionic-native';
+import {HomePage} from './pages/home/home';
+import {DrugsPage} from './pages/drugs/drugs';
+
+@Component({
+  templateUrl: 'build/app.html'
+})
+class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  // make HomePage the root (or first) page
+  rootPage: any = HomePage;
+  pages: Array<{title: string, icon:string, component: any, showchild:boolean,
+    child: Array<{title:string, icon:string, component: any}>}>;
+
+  constructor(
+    public platform: Platform,
+    public menu: MenuController
+  ) {
+    this.initializeApp();
+
+    // set our app's pages
+    this.pages = [
+      { title: 'Dashboard', icon:'add', component: HomePage , showchild:false ,child: null},
+      { title: 'Sales', icon:'pricetags', component: DrugsPage, showchild:false , child: null  },
+      { title: 'Invoice', icon:'clipboard', component: DrugsPage, showchild:false , child: null },
+      { title: 'Drug Inventory', icon:'home', showchild:false , component: DrugsPage,
+        child:[
+          { title: 'Add New Drug', icon:'add', component: DrugsPage},
+          { title: 'Edit Drug', icon: 'create', component: DrugsPage},
+          { title: 'Search Drug', icon: 'search', component: DrugsPage},
+          { title: 'Import Drugs', icon: 'swap', component: DrugsPage} ]},
+      { title: 'Master Data', icon:'build', component: DrugsPage, showchild:false , child: null  },
+      { title: 'Analytics', icon:'stats', component: DrugsPage, showchild:false , child: null },
+      { title: 'Notifications', icon:'chatboxes', component: DrugsPage, showchild:false , child: null },
+      { title: 'Tools', icon:'hammer', component: DrugsPage, showchild:false , child: null  },
+      { title: 'Book of business', icon:'logo-usd', component: DrugsPage, showchild:false , child: null  },
+      { title: 'MyAccount', icon:'person', component: DrugsPage, showchild:false , child: null },
+    ];
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
+
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    this.nav.setRoot(page.component);
+  }
+
+  toggleMenuItem(p){
+    p.showchild = !p.showchild;
+  }
+}
+
+ionicBootstrap(MyApp);
---
+++ app/pages/drugs/drugs.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  templateUrl: 'build/pages/drugs/drugs.html'
+})
+export class DrugsPage {
+
+  constructor() {
+  }
+}
